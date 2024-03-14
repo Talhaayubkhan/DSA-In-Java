817521c483c99bba886ec5511e51c4d7fb733ddc
Talha Ayub <khantalhrahi@email.com>
2024-03-15 00:43:20 +0500

    This code sorts an array of coins and finds the maximum number of coins that can be made with a given amount.

---
+++ Greedy Algorithims/CoinsSystem.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class CoinsSystem {
+     public static void main(String[] args) {
+          // Define an array of coins
+          Integer coins[] = { 1, 2, 5, 10, 20, 50, 100, 500, 1000, 2000 };
+
+          // Sort the coins in descending order
+          Arrays.sort(coins, Comparator.reverseOrder());
+
+          // Initialize variables for coin count and amount
+          int coinCount = 0;
+          int amount = 590;
+
+          // Create an ArrayList to store the coins
+          ArrayList<Integer> ans = new ArrayList<>();
+
+          // Loop through the coins
+          for (int i = 0; i < coins.length; i++) {
+               // If the current coin is less than or equal to the amount, add it to the
+               // ArrayList and update the amount and coin count
+               if (coins[i] <= amount) {
+                    // Increment the coin count
+                    coinCount++;
+                    // Add the current coin to the ArrayList
+                    ans.add(coins[i]);
+                    // Subtract the current coin from the amount
+                    amount -= coins[i];
+               }
+          }
+
+          // Print the maximum coin count and the coins in the ArrayList
+          System.out.println("Max coin count: " + coinCount);
+          for (int i = 0; i < ans.size(); i++) {
+               System.out.print(ans.get(i) + " ");
+          }
+          System.out.println();
+     }
+}
