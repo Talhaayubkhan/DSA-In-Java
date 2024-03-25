4daa2f549d73358dbacb760ef6ab4aa29fa9990c
Talha Ayub <khantalharahi@gmail.com>
2024-03-26 00:14:15 +0500

    add some good comments with each line of code

--- Greedy Algorithims/ChocolaProblem.java
+++ Greedy Algorithims/ChocolaProblem.java
@@ -2,43 +2,67 @@ import java.util.*;
 
 public class ChocolaProblem {
      public static void main(String[] args) {
+          // Number of rows (vertical cuts) and columns (horizontal cuts) of chocolates
           int n = 4, m = 6;
+
+          // Cost of each vertical cut
           Integer costVer[] = { 2, 1, 3, 1, 4 };
+          // Cost of each horizontal cut
           Integer costHor[] = { 4, 1, 2 };
 
+          // Sorting the arrays in descending order to get the most expensive cuts first
           Arrays.sort(costVer, Collections.reverseOrder());
           Arrays.sort(costHor, Collections.reverseOrder());
 
-          int h = 0, v = 0;
-          int hp = 1, vp = 1;
+          // Initial variables to track horizontal and vertical cuts, and pieces
+          int h = 0, v = 0; // Indices for horizontal and vertical cuts
+          int hp = 1, vp = 1; // Number of horizontal and vertical pieces
 
+          // Total cost of all cuts
           int cost = 0;
 
+          // Loop until one of the arrays is fully processed
           while (h < costHor.length && v < costVer.length) {
                if (costVer[v] <= costHor[h]) {
-                    // horizontal cut
+                    // Horizontal cut is cheaper than or equal to vertical cut
+                    // Add cost of horizontal cut to total cost
                     cost += (costHor[h] * vp);
+                    // Increase number of horizontal pieces
                     hp++;
+                    // Move to the next horizontal cut
                     h++;
                } else {
-                    // vertical cut
+                    // Vertical cut is cheaper than horizontal cut
+                    // Add cost of vertical cut to total cost
                     cost += (costVer[v] * hp);
+                    // Increase number of vertical pieces
                     vp++;
+                    // Move to the next vertical cut
                     v++;
                }
           }
 
+          // Process remaining horizontal cuts, if any
           while (hp <= costHor.length) {
+               // Add cost of horizontal cut to total cost
                cost += (costHor[h] * vp);
+               // Increase number of horizontal pieces
                hp++;
+               // Move to the next horizontal cut
                h++;
           }
+
+          // Process remaining vertical cuts, if any
           while (vp <= costVer.length) {
+               // Add cost of vertical cut to total cost
                cost += (costVer[v] * hp);
+               // Increase number of vertical pieces
                vp++;
+               // Move to the next vertical cut
                v++;
           }
 
-          System.out.println("cost of total cuts are " + cost);
+          // Print the total cost of all cuts
+          System.out.println("Cost of total cuts is = " + cost);
      }
 }
