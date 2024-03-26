1ab3cb549b40958f1c5740f496fe358dd3bc7f22
Talha Ayub <khantalharahi@gmail.com>
2024-03-26 23:38:47 +0500

    add the updated code

--- Greedy Algorithims/PracticeQuestion/LargestOddNumberInRange.java
+++ Greedy Algorithims/PracticeQuestion/LargestOddNumberInRange.java
@@ -1,7 +1,35 @@
 package PracticeQuestion;
 
+// import java.util.ArrayList;
 public class LargestOddNumberInRange {
+     public static int largestOddNumberInRange(int[] range, int k) {
+          if (k <= 0) {
+               return 0;
+          }
+
+          int L = range[0];
+          int R = range[1];
+
+          if ((R & 1) > 0) {
+               int count = (int) Math.ceil((R - L + 1) / 2);
+               if (k > count)
+                    return 0;
+               else
+                    return (R - 2 * k + 2);
+          } else {
+               int count = (R - L + 1) / 2;
+               if (k > count)
+                    return 0;
+               else
+                    return (R - 2 * k + 1);
+          }
+     }
+
      public static void main(String[] args) {
-          int L = -3, R = 3, K = 1;
+          int[] range = { -3, 3 };
+          int k = 1;
+
+          int largestOdd = largestOddNumberInRange(range, k);
+          System.out.println("Kth largest odd number in the range: " + largestOdd);
      }
 }
